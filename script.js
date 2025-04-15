2bbfd81393b695552fd53498a2571edb04d11f08
vvishwanath9 <vviswhanath9@gatech.edu>
2025-04-14 20:01:08 -0400

    temp commit

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let selectedItem = 0;
 function reselect(i) {
     selectedItem = i
     tocItems.forEach(b => b.style.backgroundColor = "#F8F8F8")
-    console.log(i);
     tocItems[i].style.backgroundColor = "#ABEFEF"
 }
 
@@ -24,11 +23,9 @@ const totalHeight = sections.scrollHeight - sections.clientHeight;
 sections.addEventListener('scroll', () => {
     let progress = sections.scrollTop / totalHeight;
     if (progress >= 1) progress = 0.9999;
-    console.log(progress);
     bars[0].style.height = Math.min(progress * 2000/3, 100) + '%'
     bars[1].style.height = Math.min((progress - 0.15) * 1000, 100) + '%'
     bars[2].style.height = Math.min((progress - 0.25) * 500/3, 100) + '%'
-    console.log(progress)
     if (progress < 0.15) reselect(0)
     else if(progress < 0.3) reselect(1)
     else if(progress < 0.9) reselect(2)
